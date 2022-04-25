a2fe20cb84846be5e8e5805967a390e7dbc63191
James Karr <jameskarrdev@gmail.com>
2022-04-25 16:21:03 -0500

    nasa

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// The user will enter a date. Use that date to get the NASA picture of the day from that date!
+// https://api.nasa.gov/
+
+
+
+document.querySelector('button').addEventListener('click', goFetch)
+
+function goFetch() {
+    //to add multiple parameters to url, use & between
+    const userInput = document.querySelector('input').value
+    const url = `https://api.nasa.gov/planetary/apod?api_key=mfwsXK54fgcnaTvb5YjZOzEYaXq0fNeh9bgxa0ZZ&date=${userInput}`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            document.querySelector('h2').innerText = data.title
+            document.querySelector('p').innerText = data.explanation
+            if(data.media_type === 'image') {
+                document.querySelector('img').src = data.hdurl
+              } else if(data.media_type === 'video') {
+                document.querySelector('iframe').src = data.url
+              }
+            
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+}
