490c98977362af77eff39dc7e895d95537775940
James Karr <jameskarrdev@gmail.com>
2022-05-07 20:24:11 -0500

    added css styles. added JS functionality to toggle hidden class on IMG vs IFRAME

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
-// The user will enter a date. Use that date to get the NASA picture of the day from that date!
+//The user will enter a date. Use that date to get the NASA picture of the day from that date!
 // https://api.nasa.gov/
 
 document.querySelector('button').addEventListener('click', goFetch)
 
 function goFetch() {
     const userInput = document.querySelector('input').value
-    //to add multiple parameters to url, use & between parameters
-    const url = `https://api.nasa.gov/planetary/apod?api_key=mfwsXK54fgcnaTvb5YjZOzEYaXq0fNeh9bgxa0ZZ&date=${userInput}`
+    let url = `https://api.nasa.gov/planetary/apod?api_key=mfwsXK54fgcnaTvb5YjZOzEYaXq0fNeh9bgxa0ZZ&date=${userInput}`
 
     fetch(url)
         .then(res => res.json())
         .then(data => {
             console.log(data)
             document.querySelector('h2').innerText = data.title
+            document.querySelector('h3').innerText = data.date
             document.querySelector('p').innerText = data.explanation
+            
+
             if(data.media_type === 'image') {
                 document.querySelector('img').src = data.hdurl
-              } else if(data.media_type === 'video') {
+                document.querySelector('iframe').classList.add('hidden')
+                document.querySelector('img').classList.remove('hidden')
+            } else if(data.media_type === 'video') {
                 document.querySelector('iframe').src = data.url
-              }
+                document.querySelector('img').classList.add('hidden')
+                document.querySelector('iframe').classList.remove('hidden')
+            }
+            
             
         })
         .catch(err => {
